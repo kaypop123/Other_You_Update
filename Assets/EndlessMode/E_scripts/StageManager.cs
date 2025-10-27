3535f6840ea29be9d375c3bc47fbbdcd71b44f11
Hye0nJ1 <hinzz1101@naver.com>
2025-10-27 15:36:45 +0900

    스테이지 및 스코어 조정
    1, 스테이지 번쩍 뜨게
    2. 우측 상단 스테이지 표시 지속적으로
    3. 우측 상단 스코어 표시 크기 조정

--- Assets/EndlessMode/E_scripts/StageManager.cs
+++ Assets/EndlessMode/E_scripts/StageManager.cs
@@ -7,7 +7,8 @@ public class StageManager : MonoBehaviour
 {
     public static StageManager Instance;
 
-    public TextMeshProUGUI stageText;
+    public TextMeshProUGUI stageText; // 스테이지 넘어갈 때마다 띄울 거
+    public TextMeshProUGUI stageDisplayText; // 상시로 띄울 스테이지
     public CanvasGroup stageBackground; // 반투명 배경용 CanvasGroup
 
     private int currentStage = 1;
@@ -22,6 +23,7 @@ public class StageManager : MonoBehaviour
     {
         stageText.gameObject.SetActive(false);
         stageBackground.alpha = 0; // 투명 초기화
+        UpdateStageDisplay(); // 게임 시작 시 우측 상단에 표시
 
         StartCoroutine(ShowInitialStage());
     }
@@ -29,12 +31,18 @@ public class StageManager : MonoBehaviour
     public void IncreaseStageAndShow()
     {
         currentStage++;
+        UpdateStageDisplay(); // 스테이지 올라갈 때 즉시 갱신
         StartCoroutine(ShowStageText());
     }
 
     private IEnumerator ShowInitialStage()
     {
         yield return ShowStageWithBackground(currentStage);
+    }
+
+    private void UpdateStageDisplay()
+    {
+        stageDisplayText.text = $"STAGE {currentStage}";
     }
 
     private IEnumerator ShowStageText()
@@ -50,9 +58,9 @@ public class StageManager : MonoBehaviour
 
         // 배경 Fade In
         stageBackground.DOFade(0.6f, 0.3f); // alpha 0 → 0.6
-        stageText.DOFade(1, 0.3f);
+        stageText.DOFade(1f, 0.3f);
 
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(0.9f);
 
         // Fade Out
         stageBackground.DOFade(0, 0.3f);
